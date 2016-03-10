f2d6c72bd13cd3a4459bed1e3839fd198c537896
Hanneybadger <hwillen@gmail.com>
2016-03-10 22:21:10 +0100

    Fått chartist att funka!
    Har skapat en bower.json fil som hanterar dependencies i applikationen.
    Och har lagt till Chartist i bower.json-filen som gör att vi nu kan
    lägga in .js-filer från chartist så att de funkar och går att meditera
    :D

---
+++ js/charttest.js
@@ -0,0 +1,14 @@
+new Chartist.Line('.ct-chart', {
+  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+  series: [
+    [12, 9, 7, 8, 5],
+    [2, 1, 3.5, 7, 3],
+    [1, 3, 4, 5, 6],
+    [7, 5, 43, 3, 7]
+  ]
+}, {
+  fullWidth: true,
+  chartPadding: {
+    right: 40
+  }
+});
